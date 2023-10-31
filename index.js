7e1f0504e7bd4e037d1c3d87fc57d785a5fe85e4
RaykujanK13 <lancarjaya1928@gmail.com>
2023-10-31 15:39:32 -0700

    Anantarupa
    Lyfrith Update 10/1
    
    [Update] generateQRCode
    [ADDED] Login Staff

--- index.js
+++ index.js
@@ -15,30 +15,75 @@ const port = 3000;
 
 app.use(express.json());
 
+
+//staff login = belum bisa
+app.post("/login", async (req, res) => {
+  const { passcode } = req.body;
+
+  try {
+    const passcodeSnapshot = await db.collection("staffpasscode").doc(passcode.toString()).get();
+
+    if (passcodeSnapshot.exists) {
+      res.status(200).json({ message: "Login Succes" });
+    } else {
+      res.status(401).json({ message: "Login Failed, passcode Not valid" });
+    }
+  } catch (error) {
+    console.error("Error: ", error);
+    res.status(500).json({ message: "Error Server Get Collection" });
+  }
+});
+
+
 // Generate QR code
 app.get("/generateQRCode/:ticketNumber", async (req, res) => {
   const ticketNumber = req.params.ticketNumber;
 
-  const ticketRef = db.collection("tickets").where("TicketNumber", "==", ticketNumber);
-  const querySnapshot = await ticketRef.get();
+  try {
+    const ticketRef = db.collection("tickets");
+    const querySnapshot = await ticketRef.where("TicketNumber", "==", ticketNumber).get();
 
-  if (querySnapshot.empty) {
-    res.status(404).send("Ticket not found");
-  } else {
-    const ticketData = querySnapshot.docs[0].data();
-    const qrData = JSON.stringify(ticketData);
+    if (querySnapshot.empty) {
+      res.status(404).send("Ticket not found");
+    } else {
+      const ticketData = querySnapshot.docs[0].data();
+      const {
+        BookingTimestamp,
+        CreatedDate,
+        CustomerEmail,
+        CustomerId,
+        CustomerName,
+        ExpiredTimestamp,
+        IsUsed,
+        OrderId,
+        ProductId,
+        ProductName,
+        TicketNumber,
+        VariantName,
+        VariantType
+      } = ticketData;
 
-    const canvas = createCanvas(200, 200);
-    qrcode.toCanvas(canvas, qrData, (error) => {
-      if (error) {
-        console.error(error);
-        res.status(500).send("Error generating QR code");
-      } else {
-        const stream = canvas.createPNGStream();
-        res.type("png");
-        stream.pipe(res);
-      }
-    });
+      const responseData = {
+        BookingTimestamp,
+        CreatedDate,
+        CustomerEmail,
+        CustomerId,
+        CustomerName,
+        ExpiredTimestamp,
+        IsUsed,
+        OrderId,
+        ProductId,
+        ProductName,
+        TicketNumber,
+        VariantName,
+        VariantType
+      };
+
+      res.json(responseData);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error retrieving ticket data");
   }
 });
 
@@ -51,21 +96,28 @@ app.post("/validateQRCode", async (req, res) => {
     res.status(404).send("Ticket not found");
   } else {
     const validatedTicket = ticketData.docs[0].data();
-    
-    const validateData = {
-      Status: "Active",
-      ScanTimestamp: new Date(), 
-      VisitorCount: 1, 
-      ...validatedTicket, 
-    };
 
-    await db.collection("ticketsvalidate").add(validateData);
+    const validateQuery = await db.collection("ticketsvalidate").where("TicketNumber", "==", qrData.TicketNumber).get();
+
+    if (validateQuery.empty) {
+      const validateData = {
+        Status: "Active",
+        ScanTimestamp: new Date(),
+        VisitorCount: 1, 
+        ...validatedTicket,
+      };
+      await db.collection("ticketsvalidate").add(validateData);
+    } else {
+      const validateDoc = validateQuery.docs[0];
+      const validateData = validateDoc.data();
+      validateData.VisitorCount += 1;
+      await validateDoc.ref.update(validateData);
+    }
 
     res.json(validateData);
   }
 });
 
-  
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
